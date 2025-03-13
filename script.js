0078e8a90c50f79392c3036c960ebc346eae41a7
charitypepe <info@charitypepe.com>
2025-03-13 17:44:55 +0100

    Initial upload of Web3ChainLabsAI website

--- script.js
+++ script.js
@@ -1,13 +1,52 @@
-document.getElementById('connectWallet').addEventListener('click', async () => {
+const tokenAddress = "0xYOUR_TOKEN_ADDRESS";  // Трябва да се замени с реалния адрес след деплой
+const tokenABI = [
+    {
+        "constant": true,
+        "inputs": [{"name": "owner", "type": "address"}],
+        "name": "balanceOf",
+        "outputs": [{"name": "balance", "type": "uint256"}],
+        "type": "function"
+    }
+];
+
+async function connectWallet() {
     if (window.ethereum) {
         try {
-            const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-            alert(`Connected: ${accounts[0]}`);
+            const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+            document.getElementById('walletAddress').innerText = `Connected: ${accounts[0]}`;
+            checkTokenBalance(accounts[0]);
         } catch (error) {
             console.error(error);
-            alert('Connection failed.');
+            alert('Wallet connection failed.');
         }
     } else {
-        alert('Please install MetaMask.');
+        alert('Please install MetaMask!');
     }
-});
+}
+
+async function checkTokenBalance(userAddress) {
+    const web3 = new Web3(window.ethereum);
+    const contract = new web3.eth.Contract(tokenABI, tokenAddress);
+    const balance = await contract.methods.balanceOf(userAddress).call();
+    const tokenBalance = web3.utils.fromWei(balance, 'ether');
+
+    alert(`Your $W3LABS Balance: ${tokenBalance} tokens`);
+
+    // Изпращаме данните към Telegram бота
+    sendBalanceToTelegram(userAddress, tokenBalance);
+}
+
+async function sendBalanceToTelegram(userAddress, tokenBalance) {
+    const telegramBotURL = "https://api.telegram.org/botYOUR_TELEGRAM_BOT_TOKEN/sendMessage";
+    
+    const message = `User: ${userAddress}\n$W3LABS Balance: ${tokenBalance}`;
+    
+    await fetch(telegramBotURL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            chat_id: "@Web3ChainLabsAI",
+            text: message
+        })
+    });
+}
